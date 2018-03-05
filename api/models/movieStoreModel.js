5ab01ced7e2bf17d6921d4b20afd1b4d26a0950b
Alexander Barriga <45.al3x@gmail.com>
2018-03-04 22:32:06 -0600

    Added API files, created movieStoreModel, configured package.json

---
+++ api/models/movieStoreModel.js
@@ -0,0 +1,23 @@
+'use strict';
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+
+var movieSchema = new Schema({
+    name: {
+        type: String,
+        required: 'Kindly enter the name of the movie'
+    },
+    directedBy: {
+        type: String,
+        default: ''
+    },
+    duration: {
+        type: String,
+        required: 'Kindly enter the duration of the movie'
+    },
+    photo: {
+        type: String,
+    }
+});
+
+module.exports = mongoose.model('Movies', movieSchema);
